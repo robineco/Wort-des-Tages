073cacd5347159b37f7a4ff97069ffb015e2e7cc
Robin Schlund <rschlund@stud.hs-heilbronn.de>
2020-06-17 20:22:25 +0200

    more functions added

--- word.py
+++ word.py
@@ -3,23 +3,74 @@ from termcolor import colored
 from bs4 import BeautifulSoup
 import requests
 
-try:
-    url = 'https://www.duden.de/wort-des-tages'
-    response = requests.get(url)
-
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    block = soup.find(id='block-wordoftheday').findAll('a')
-    word = block[0].contents[0].replace('\u00AD', '')
-    info = block[1].contents[0][:-2].replace('\u00AD', '')
+lenOfLongestText = 5
 
 
+def createFrequency(full, empty):
+    wordFrequency = ''
+    for i in range(full):
+            wordFrequency += '▮'
+        
+    for i in range(empty):
+        wordFrequency += '▯'
+    
+    return wordFrequency
+
+
+def updateLongestText(text):
+    global lenOfLongestText
+    if len(text) > lenOfLongestText:
+        lenOfLongestText = len(text)
+
+
+def printSeperator():
     minus = ''
-    for i in range(16 + len(info)):
+    for i in range(16 + lenOfLongestText):
         minus += '-'
+    print(colored(minus, 'yellow'))
 
-    print(colored(minus, 'yellow'))
-    print(colored('Wort des Tages: ', 'green') + colored(word, 'cyan', attrs=['bold']) + '\n' + colored('Bedeutung:      ', 'green') + colored(info, 'cyan'))
-    print(colored(minus, 'yellow'))
-except ConnectionError as e:
-    print(colored('No Internet Connection', 'red', attrs=['bold', 'blink']))
+
+def main():
+    try:
+        baseUrl = 'https://www.duden.de'
+        wordOfTheDayUrl = '/wort-des-tages'
+
+        response = requests.get(baseUrl + wordOfTheDayUrl)
+
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        block = soup.find(id='block-wordoftheday').find('a').attrs['href']
+        wordUrl = baseUrl + block
+
+        response = requests.get(wordUrl, "html.parser")
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        
+        word = soup.find("span", {"class": "lemma__main"}).text
+        word = word.replace('\u00AD', '')
+        updateLongestText(word)
+
+        full = len(soup.find("span", {"class": "shaft__full"}).text)
+        empty = len(soup.find("span", {"class": "shaft__empty"}).text)
+        wordFrequency = createFrequency(full, empty)
+        
+        rechtschreibung = soup.find(id='rechtschreibung').find('dd').text
+        updateLongestText(rechtschreibung)
+        bedeutung = soup.find(id='bedeutung').find('p').text
+        updateLongestText(bedeutung)
+        herkunft = soup.find(id='herkunft').find('p').text
+        updateLongestText(herkunft)
+
+        printSeperator()
+        print(colored('Wort des Tages: ', 'green') + colored(word, 'cyan'))
+        print(colored('Häufigkeit:     ', 'green') + colored(wordFrequency, 'cyan'))
+        print(colored('Worttrennung:   ', 'green') + colored(rechtschreibung, 'cyan'))
+        print(colored('Bedeutung:      ', 'green') + colored(bedeutung, 'cyan'))
+        print(colored('Herkunft:       ', 'green') + colored(herkunft, 'cyan'))
+        printSeperator()
+
+    except ConnectionError as e:
+        print(colored('No Internet Connection', 'red', attrs=['bold', 'blink']))
+
+if __name__ == "__main__":
+    main()
